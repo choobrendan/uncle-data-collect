feb8f5eaa3fc8b85fcc7c6f61f0a9d3328f44e24
choobrendan <choobrendan@gmail.com>
2025-03-14 12:30:47 +0800

    Updates

---
+++ static/js/952.6bd63e3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdata_collect=self.webpackChunkdata_collect||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.6bd63e3c.chunk.js.map
